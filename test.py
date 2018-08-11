38db151fc1a08e4ece32c58ee7a8a9a617244384
Robert Xiao <brx@cs.cmu.edu>
2018-08-11 10:27:42 -0700

    PDP-1

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import sys
 
 amd64 = open('shellcode.amd64', 'rb').read()
 
-bs = BitString([None] * (512 * 8))
+bs = BitString([None] * (4096 * 8))
 # bs.write_safe(0, None, bytes_to_bits('\xeb\x7a')) # [u]nconditional jump = 0xa for lgp30
 # jmp for PDP-8
 bs.write_safe(0, 3, 5)
@@ -23,23 +23,41 @@ bs.write_safe(24, 6, 2)
 bs.write_safe(3, 9, 0140)
 # jmp address for LGP-30
 bs.write_safe(18, 6, 2)
+# jmp for PDP-1
+bs.write_safe(18*2, 18, 0622554)
 
 # program for LGP-30
 lgp30.build_prog(bs, 31*(64+2)) # @1984 for 248
 
-# program for MIX
-mix.build_prog(bs, 30)
+# mix nops
+bs.write_safe(30+24, 6, 0)
+bs.write_safe(60+24, 6, 0)
+bs.write_safe(90+24, 6, 0)
+mix.build_prog(bs, 120)
 
-# jump for arm64
-bs.write_safe(60, 4, 0x01)
+# arm "and" instr
+bs.write_safe(28, 4, 0xa)
+bs.write_safe(32+28, 4, 0xe)
+bs.write_safe(32, 24, 0xcc95b0)
 offs = (0x200 - 8) >> 2
 bs.write_safe(64, 32, struct.unpack('>I', struct.pack('<I', 0x14000000 + offs))[0])
 bs.write_safe(0x200 * 8, None, bytes_to_bits(open('shellcode.arm64', 'rb').read()))
 
+# clemency???
+bs.write_safe(36, 9, 0b110010010)
+
+out = ''
+for i in xrange(0, len(bs), 3):
+    out += str(int(''.join(random.choice('01') if c is None else c for c in bs[i:i+3]), 2))
+    if i % 18 == 15: out += ' '
+print >> sys.stderr, out
+
+print >> sys.stderr, BitString.repr(bs[:81])
+
 # program for PDP-8
 pdp8.build_prog(bs, 12*040) # @384 for 180
 
-print >> sys.stderr, bs
+bs.write_safe((02554-64)*18, None, '010011000000111111100100000000111111110110010000111111111011000000000011110110010000111111111011000000000011110110010000111111111011000000000011110000000001000000000000')
 
 out = ''
 for i in xrange(0, len(bs), 8):
